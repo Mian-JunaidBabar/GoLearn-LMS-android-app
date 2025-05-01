27313f68f8d99d1fab4f6e3a713ca101c47e2178
Junaid Babar <junaidbabar125@gmail.com>
2025-05-01 18:18:35 +0500

    just added a tollbar to activity assignment detail

--- app/src/main/java/com/example/mad_project/AssignmentDetailActivity.java
+++ app/src/main/java/com/example/mad_project/AssignmentDetailActivity.java
@@ -5,6 +5,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class AssignmentDetailActivity extends AppCompatActivity {
 
@@ -15,6 +16,9 @@ public class AssignmentDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment_detail);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); // Use setSupportActionBar for androidx Toolbar
 
         titleText = findViewById(R.id.assignment_title);
         descriptionText = findViewById(R.id.assignment_description);
