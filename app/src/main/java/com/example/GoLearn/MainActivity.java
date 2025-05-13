71ffa8016677a30954b348cb7b48f92e900682de
Junaid Babar <junaidbabar125@gmail.com>
2025-05-13 21:09:15 +0500

    fixes, app now works even in dark theme

--- app/src/main/java/com/example/GoLearn/MainActivity.java
+++ app/src/main/java/com/example/GoLearn/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
